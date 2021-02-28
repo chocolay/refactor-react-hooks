1dba7f57dc7eb990c921acc64714d0fe12b47b0f
Elise A. Ralph <elise.ralph@gmail.com>
2021-02-28 15:49:40 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- src/features/Dashboard/SummaryContainer.js
+++ src/features/Dashboard/SummaryContainer.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { useContext } from "react"
+import { globalContext } from "../../App"
 
-const SummaryContainer = ({ salesTotal, subscriptionsTotal }) => {
+export default function SummaryContainer() {
+        const { salesTotal, subscriptionsTotal } = useContext(globalContext)
+
   return (
     <div className="summary flex flex-row">
       <div className="card bg-indigo">
@@ -14,19 +15,5 @@ const SummaryContainer = ({ salesTotal, subscriptionsTotal }) => {
         <p>$ {subscriptionsTotal}</p>
       </div>
     </div>
-  );
-};
-
-const mapStateToProps = state => {
-  return {
-    salesTotal: state.dataset.salesTotal,
-    subscriptionsTotal: state.dataset.subscriptionsTotal
-  };
-};
-
-SummaryContainer.propTypes = {
-  salesTotal: PropTypes.number.isRequired,
-  subscriptionsTotal: PropTypes.number.isRequired
-};
-
-export default connect(mapStateToProps)(SummaryContainer);
+  )
+}//SummaryContainer
